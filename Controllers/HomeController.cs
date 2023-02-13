e21084ba37416694d46db1860bfdb331cb845214
rodolfofadino <rodolfo.fadino@gmail.com>
2023-02-13 15:07:45 -0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using fiap.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fiap.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["aluno"] = $"Ana Lucia {DateTime.Now.ToString()}";
+
+            ViewBag.TipoDoProduto = "Tenis";
+
+            //return View("Produto");
+
+            //abstraindo o acesso ao banco
+            var homeModel = new HomeViewModel();
+            homeModel.CategoriaDoProduto = "Calcados";
+            homeModel.NomeDoAluno = "Caio";
+
+         
+            return View(homeModel);
+        }
+
+        public IActionResult Detalhe(string categoria, int idDoProduto, string tamanho)
+        {
+            return View("Produto");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews();
+//builder.Services.AddControllers();
+
+var app = builder.Build();
+
+
+app.UseStaticFiles();
+
+
+//https://localhost:59148/home/index
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "padraodoproduto",
+    defaults: new { Controller = "Home", Action = "Detalhe" },
+    pattern: "{categoria}/seuperfil/{idDoProduto}"
+);
+
+app.MapControllerRoute(
+    name: "default",
+    defaults: new {Controller = "Home", Action = "Index" },
+    pattern: "{controller}/{action}/{id?}"
+);
+//app.MapControllerRoute(
+//    name: "default",
+//   // pattern: "{controller}/{action}/{id?}"
+//    pattern: "{controller=Home}/{action=Index}/{id?}"
+//);
+
+
+
+
+
+//app.MapGet("/", 
+
+//    () => "Boa noite agora que tem energia"
+
+//    );
+//app.MapGet("/teste", () => "teste");
+
+
+app.Run();
+
+
+
+
+
