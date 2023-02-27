c7b5b39c008333fe183873a31413b77ed6f4ce5f
rodolfofadino <rodolfo.fadino@gmail.com>
2023-02-27 19:52:01 -0300

    final crud musicas

--- Controllers/MusicasController.cs
+++ Controllers/MusicasController.cs
@@ -1,9 +1,11 @@
 ﻿using fiap.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 
 namespace fiap.Controllers
 {
-  
+
     public class MusicasController : Controller
     {
         private MusicaContext _context;
@@ -12,7 +14,14 @@ namespace fiap.Controllers
         {
             _context = context;
         }
-        
+
+        public IActionResult Index()
+        {
+            var musicas = _context.Musicas.ToList();
+
+            return View(musicas);
+        }
+
         [HttpGet]
         public IActionResult Create()
         {
@@ -20,12 +29,11 @@ namespace fiap.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create([FromForm]Musica model)
+        public IActionResult Create([FromForm] Musica model)
         {
             if (ModelState.IsValid)
             {
                 //salva no banco
-
                 _context.Musicas.Add(model);
                 _context.SaveChanges();
 
@@ -34,6 +42,74 @@ namespace fiap.Controllers
 
 
             return View(model);
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            var musica = _context.Musicas.Find(Id);
+
+            return View(musica);
+        }
+
+        [HttpPost]
+        public IActionResult Edit([FromForm] Musica model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(model);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int Id)
+        {
+            return View(_context.Musicas.First(a => a.Id == Id));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int Id)
+        {
+            #region exemplos tasks
+            //var musica = _context.Musicas.Single(a => a.Id == Id);
+            //Select top 2 * from musicas where id =1
+
+            //var musica1 = _context.Musicas.SingleOrDefaultAsync(a => a.Id == Id + 1);
+            //var musica2 = _context.Musicas.SingleOrDefaultAsync(a => a.Id == Id + 2);
+            //var musica3 = _context.Musicas.SingleOrDefaultAsync(a => a.Id == Id + 3);
+
+            //List<Task> tasks = new List<Task>() { musica, musica1, musica2, musica3 };
+
+            //Task.WaitAll(tasks.ToArray());
+            //Task.WaitAny(tasks.ToArray());
+            ////null
+
+            //var musica = _context.Musicas.First(a => a.Id == Id);
+            ////select top 1
+
+            //var musica = _context.Musicas.FirstOrDefault(a => a.Id == Id);
+            ////null
+            #endregion
+
+            var musica = await _context.Musicas.SingleOrDefaultAsync(a => a.Id == Id);
+            if (musica != null)
+            {
+                _context.Musicas.Remove(musica);
+                // _context.SaveChanges();
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+
+            }
+
+
+            return new NotFoundResult();
         }
     }
 }
