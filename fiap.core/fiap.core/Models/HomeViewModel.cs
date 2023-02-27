e8d6df44a6537f1934e10ce5cd5b1802e0a10751
rodolfofadino <rodolfo.fadino@gmail.com>
2023-02-27 20:59:09 -0300

    move EF e models to ClassLibrary

---
+++ fiap.core/fiap.core/Models/HomeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace fiap.core.Models
+{
+    public class HomeViewModel
+    {
+        public string NomeDoAluno { get; set; }
+        public string CategoriaDoProduto { get; set; }
+    }
+}
---
+++ fiap.core/fiap.core/Models/Musica.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace fiap.core.Models
+{
+    public class Musica
+    {
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Titulo { get; set; }
+        public int? Tempo{ get; set; }
+        public string Banda{ get; set; }
+        public string Album{ get; set; }
+        public string EstiloMusical { get; set; }
+    }
+}
