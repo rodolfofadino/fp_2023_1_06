e76605837697eef6681d5adeb28542ded9cd365d
rodolfofadino <rodolfo.fadino@gmail.com>
2023-02-27 21:48:41 -0300

    add basic api endpoint

---
+++ fiap.api/fiap.api/Controllers/MusicasController.cs
@@ -0,0 +1,114 @@
+﻿using fiap.core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace fiap.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicasController : Controller
+    {
+        private MusicaContext _context;
+
+        public MusicasController(MusicaContext context)
+        {
+            _context = context;
+        }
+        //[HttpGet]
+        //public IActionResult Get()
+        //{
+        //    var musicas = new List<Musica>() { new Musica(), new Musica() };
+
+        //    return Ok( musicas);
+        //}
+
+
+
+        //[HttpGet]
+        //public ActionResult<List<Musica>> Get()
+        //{
+        //    var musicas = new List<Musica>() { new Musica(), new Musica() };
+
+        //    return Ok(musicas);
+        //}
+
+
+
+        //[HttpGet]
+        //public List<Musica> Get()
+        //{
+        //    var musicas = new List<Musica>() { new Musica(), new Musica() };
+
+        //    //if (!musicas.Any())
+        //    //{
+        //    //    return NotFound();
+        //    //}
+
+        //    return musicas;
+        //}
+
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(List<Musica>))]
+        [ProducesResponseType(400)]
+        public async Task<ActionResult<List<Musica>>> Get()
+        {
+
+            return Ok(await _context.Musicas.ToListAsync());
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        public async Task<ActionResult<Musica>> Get(int id)
+        {
+            var musica = await _context.Musicas.FirstOrDefaultAsync(a => a.Id == id);
+            if (musica == null)
+                return NotFound();
+
+            return Ok(musica);
+        }
+
+        [HttpPost]
+        //public async Task<ActionResult<Musica>> Post([FromBody]Musica model)
+        public async Task<ActionResult<Musica>> Post(Musica model)
+        {
+            //if (ModelState.IsValid)
+            //{
+            _context.Musicas.Add(model);
+            await _context.SaveChangesAsync();
+
+            return Created($"/api/musicas/{model.Id}", model);
+            //}
+
+            //return BadRequest(ModelState);
+        }
+
+
+        //[HttpPut]
+        [Route("{id}")]
+        public async Task<ActionResult<Musica>> Put(Musica model)
+        {
+            _context.Update(model);
+            await _context.SaveChangesAsync();
+            
+            return model;
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var musica = await _context.Musicas.FirstOrDefaultAsync(a => a.Id == id);
+            if (musica == null)
+                return NotFound();
+
+            _context.Remove(musica);
+            //_context.Musicas.Remove(musica);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ fiap.api/fiap.api/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using fiap.core.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connection = "Server=(localdb)\\mssqllocaldb;Database=fiap-musicas;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+builder.Services.AddDbContext<MusicaContext>(o => o.UseSqlServer(connection));
+
+builder.Services.AddControllers();
+
+
+var app = builder.Build();
+app.UseRouting();
+app.UseEndpoints(endpoints => endpoints.MapControllers());
+app.Run();
